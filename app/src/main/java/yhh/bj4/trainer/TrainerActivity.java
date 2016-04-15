96adc4dfd8e8468a48468c167a1860c754248570
Yen-Hsun, Huang <s011208@gmail.com>
2016-04-16 01:19:44 +0800

    Initial commit

---
+++ app/src/main/java/yhh/bj4/trainer/TrainerActivity.java
@@ -0,0 +1,13 @@
+package yhh.bj4.trainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trainer);
+    }
+}
