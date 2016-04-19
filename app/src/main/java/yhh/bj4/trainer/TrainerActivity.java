b76d0fa5e5b1905f6217eff150abcd6072e08de7
Yen-Hsun_Huang <yen-hsun_huang@asus.com>
2016-04-19 13:19:44 +0800

    show pager content

--- app/src/main/java/yhh/bj4/trainer/TrainerActivity.java
+++ app/src/main/java/yhh/bj4/trainer/TrainerActivity.java
@@ -1,13 +1,15 @@
 package yhh.bj4.trainer;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.PagerTitleStrip;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
-import yhh.bj4.trainer.calendar.CalendarFragment;
 import yhh.bj4.trainer.timer.TimerListFragment;
 
 public class TrainerActivity extends TransparentActivity {
@@ -15,14 +17,17 @@ public class TrainerActivity extends TransparentActivity {
     private static final String TAG = "QQQQ";
 
     private ViewPager mViewPager;
-    private PagerAdapter mPagerAdapter;
-    private PagerTitleStrip mPagerTitleStrip;
+    private TrainerPagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trainer);
+        initComponents();
+        initFragments();
+    }
 
+    private void initFragments() {
 //        if (savedInstanceState == null) {
 //            getFragmentManager().beginTransaction().replace(R.id.container, new CalendarFragment()).commitAllowingStateLoss();
 //        }
@@ -30,23 +35,45 @@ public class TrainerActivity extends TransparentActivity {
 
     private void initComponents() {
         mViewPager = (ViewPager) findViewById(R.id.pager);
-        mPagerTitleStrip = (PagerTitleStrip) findViewById(R.id.pager_title_strip);
+        mPagerAdapter = new TrainerPagerAdapter(TrainerActivity.this);
+        mViewPager.setAdapter(mPagerAdapter);
     }
 
     private static class TrainerPagerAdapter extends PagerAdapter {
+        private Activity mActivity;
+
+        public TrainerPagerAdapter(Activity activity) {
+            mActivity = activity;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "hi" + position;
+        }
+
         @Override
         public int getCount() {
             return 3;
         }
 
         @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+        @Override
         public Object instantiateItem(ViewGroup container, int position) {
-            return super.instantiateItem(container, position);
+            FrameLayout root = new FrameLayout(mActivity);
+            root.setId(position + 1);
+            mActivity.getFragmentManager().beginTransaction().replace(root.getId(), new TimerListFragment()).commitAllowingStateLoss();
+            container.addView(root);
+            return root;
         }
 
         @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return false;
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == arg1;
         }
     }
 }
--- app/src/main/java/yhh/bj4/trainer/Utilities.java
+++ app/src/main/java/yhh/bj4/trainer/Utilities.java
@@ -1,8 +1,30 @@
 package yhh.bj4.trainer;
 
+import android.os.Build;
+import android.view.View;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by User on 2016/4/18.
  */
 public class Utilities {
     public static final boolean DEBUG = true;
+
+    private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
+
+    public static int generateViewId() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+            return View.generateViewId();
+        }
+        for (; ; ) {
+            final int result = sNextGeneratedId.get();
+            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+            int newValue = result + 1;
+            if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+            if (sNextGeneratedId.compareAndSet(result, newValue)) {
+                return result;
+            }
+        }
+    }
 }
