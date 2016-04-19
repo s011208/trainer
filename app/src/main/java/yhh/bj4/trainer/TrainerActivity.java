3ef82594e32407ded60870e51a558ac7d987d5cb
s011208 <s011208@gmail.com>
2016-04-19 23:40:19 +0800

    add icons & finish tab layout

--- app/src/main/java/yhh/bj4/trainer/TrainerActivity.java
+++ app/src/main/java/yhh/bj4/trainer/TrainerActivity.java
@@ -3,6 +3,8 @@ package yhh.bj4.trainer;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class TrainerActivity extends TransparentActivity {
     private static final boolean DEBUG = Utilities.DEBUG;
@@ -11,6 +13,8 @@ public class TrainerActivity extends TransparentActivity {
     private ViewPager mViewPager;
     private TrainerPagerAdapter mPagerAdapter;
     private TrainerActivityTabLayout mTopTabLayout;
+
+    private ImageView mSchedule, mCalendar, mTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +31,11 @@ public class TrainerActivity extends TransparentActivity {
     }
 
     private void initComponents() {
+        mTopTabLayout = (TrainerActivityTabLayout) findViewById(R.id.top_tab_container);
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                Log.d("QQQQ", "position: " + position + ", positionOffset: " + positionOffset + ", positionOffsetPixels: " + positionOffsetPixels);
                 mTopTabLayout.onPageScrolled(position, positionOffset, positionOffsetPixels);
             }
 
@@ -42,11 +46,31 @@ public class TrainerActivity extends TransparentActivity {
 
             @Override
             public void onPageScrollStateChanged(int state) {
-
+                mTopTabLayout.onPageScrollStateChanged(state);
             }
         });
         mPagerAdapter = new TrainerPagerAdapter(TrainerActivity.this);
         mViewPager.setAdapter(mPagerAdapter);
-        mTopTabLayout = (TrainerActivityTabLayout) findViewById(R.id.top_tab_container);
+        mSchedule = (ImageView) findViewById(R.id.pager_schedule);
+        mSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(0, true);
+            }
+        });
+        mCalendar = (ImageView) findViewById(R.id.pager_calendar);
+        mCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(1, true);
+            }
+        });
+        mTimer = (ImageView) findViewById(R.id.pager_timer);
+        mTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(2, true);
+            }
+        });
     }
 }
--- app/src/main/java/yhh/bj4/trainer/TrainerActivityTabLayout.java
+++ app/src/main/java/yhh/bj4/trainer/TrainerActivityTabLayout.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
@@ -11,8 +13,22 @@ import android.widget.LinearLayout;
  * Created by Yen-Hsun_Huang on 2016/4/19.
  */
 public class TrainerActivityTabLayout extends LinearLayout {
+    private static final int POST_TIME = 30;
+
     private final Paint mPagePaint = new Paint();
+    private final Paint mFocusedPaint = new Paint();
     private int mCurrentPosition = 0;
+    private float mCurrentPositionOffset = 0;
+
+    private int mPostPosition = 0;
+    private float mPostPositionOffset = 0;
+    private final int mTabLayoutPaddingBottom;
+
+    private static final float THRESHOLD_OFFSET = 0.5f;
+    private boolean mScrollStarted, mCheckDirection;
+    private boolean mScrollToLeft = false;
+
+    private Handler mHandler = new Handler();
 
     public TrainerActivityTabLayout(Context context) {
         this(context, null);
@@ -24,21 +40,60 @@ public class TrainerActivityTabLayout extends LinearLayout {
 
     public TrainerActivityTabLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        mPagePaint.setColor(Color.RED);
-        mPagePaint.setStrokeWidth(20);
+        mTabLayoutPaddingBottom = context.getResources().getDimensionPixelSize(R.dimen.trainer_activity_top_tab_container_padding_bottom);
+        mPagePaint.setColor(context.getResources().getColor(R.color.colorPrimary));
+        mPagePaint.setStrokeWidth(mTabLayoutPaddingBottom);
         mPagePaint.setStyle(Paint.Style.FILL);
         mPagePaint.setAntiAlias(true);
+
+        mFocusedPaint.setColor(Color.argb(30, 150, 150, 150));
+        mFocusedPaint.setAntiAlias(true);
     }
 
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    public void onPageScrolled(final int position, final float positionOffset, final int positionOffsetPixels) {
+        if (mCheckDirection) {
+            if (THRESHOLD_OFFSET > positionOffset) {
+                mScrollToLeft = true;
+            } else {
+                mScrollToLeft = false;
+            }
+            mPostPosition = position;
+            mPostPositionOffset = positionOffset;
+            mCheckDirection = false;
+        }
         mCurrentPosition = position;
+        mCurrentPositionOffset = positionOffset;
         invalidate();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mPostPosition = position;
+                mPostPositionOffset = positionOffset;
+                invalidate();
+            }
+        }, POST_TIME);
+    }
+
+    public void onPageScrollStateChanged(int state) {
+        if (!mScrollStarted && state == ViewPager.SCROLL_STATE_DRAGGING) {
+            mScrollStarted = true;
+            mCheckDirection = true;
+        } else {
+            mScrollStarted = false;
+        }
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+    protected void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);
         if (getChildCount() == 0) return;
-        canvas.drawLine(0, 0, getChildAt(mCurrentPosition).getWidth(), 0, mPagePaint);
+        int width = getWidth() / getChildCount();
+
+        int postStartX = getWidth() / getChildCount() * mPostPosition + (int) (width * mPostPositionOffset);
+        int postFinalX = postStartX + width;
+        int currentStartX = getWidth() / getChildCount() * mCurrentPosition + (int) (width * mCurrentPositionOffset);
+        int currentFinalX = currentStartX + width;
+        canvas.drawRect(postStartX, 0, postFinalX, getHeight(), mFocusedPaint);
+        canvas.drawLine(currentStartX, getHeight() - mTabLayoutPaddingBottom, currentFinalX, getHeight() - mTabLayoutPaddingBottom, mPagePaint);
     }
 }
